5ffc7d768de9f7d85a4a5c7b9c6e6db5b578b72e
pablogarcia77 <pabloricotero98@gmail.com>
2021-09-21 15:57:04 -0300

    Item 5 add forms Familia, Categoria, UM

---
+++ Vistas/Familia/NuevaFamilia.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Vistas.Familia
+{
+    /// <summary>
+    /// Lógica de interacción para NuevaFamilia.xaml
+    /// </summary>
+    public partial class NuevaFamilia : Window
+    {
+        public NuevaFamilia()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCancalear_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnAceptar_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
