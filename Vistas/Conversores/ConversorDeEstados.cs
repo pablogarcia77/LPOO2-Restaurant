13861a9ad770a6dc1704d785e5a991d4fcf64a0a
pablogarcia77 <pabloricotero98@gmail.com>
2021-10-14 19:03:14 -0300

    Add item 3

---
+++ Vistas/Conversores/ConversorDeEstados.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace Vistas.Conversores
+{
+    
+    public class ConversorDeEstados: IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            String estado = (String)value;
+
+            if (estado == "Libre")
+            {
+                return new SolidColorBrush(Colors.Green);
+            }
+            else if (estado == "Reservada")
+            {
+                return new SolidColorBrush(Colors.Chocolate);
+            }
+            else if (estado == "Ocupada")
+            {
+                return new SolidColorBrush(Colors.Red);
+            }
+            else if (estado == "Pidiendo")
+            {
+                return new SolidColorBrush(Colors.Purple);
+            }
+            else if (estado == "En espera de comida")
+            {
+                return new SolidColorBrush(Colors.PaleGreen);
+            }
+            else if (estado == "Servidos")
+            {
+                return new SolidColorBrush(Colors.Coral);
+            }
+            else if (estado == "Esperando cuenta")
+            {
+                return new SolidColorBrush(Colors.LightBlue);
+            }
+            else if (estado == "Pagando")
+            {
+                return new SolidColorBrush(Colors.YellowGreen);
+            }
+            else
+            {
+                return new SolidColorBrush(Colors.Wheat);
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+                throw new InvalidOperationException("Conversión en un solo sentido.");
+        }
+    }
+}
--- Vistas/obj/x86/Debug/Vistas_Content.g.i.cs
+++ Vistas/obj/x86/Debug/Vistas_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("images/fondo.png")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("mesas/estadosmesa.xml")]
 
 
